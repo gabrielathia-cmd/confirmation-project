191e1cef02890cd8bdcf8c4cda961e4d9c661057
gabrielathia <g.moraesathia@gmail.com>
2026-01-27 10:15:54 -0300

    First Commit

---
+++ src/main/java/net/nsaglobal/confirmationproject/ConfirmationProjectApplication.java
@@ -0,0 +1,13 @@
+package net.nsaglobal.confirmationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfirmationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfirmationProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/nsaglobal/confirmationproject/ConfirmationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nsaglobal.confirmationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfirmationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
